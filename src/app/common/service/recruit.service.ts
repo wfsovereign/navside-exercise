63efad3bd823bf0b555c137336782fdc7ab5c521
wfsovereign <wfsovereign@outlook.com>
2017-02-03 17:14:45 +0800

    [fyqj] add recruit service layer

---
+++ src/app/common/service/recruit.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { RecruitApi } from '../api/recruit.api';
+import { Observable } from 'rxjs';
+@Injectable()
+export class RecruitService {
+
+  constructor(private recruitApi: RecruitApi) {
+
+  }
+
+  getRecruitInfo(): Observable<any> {
+    return this.recruitApi.getRecruitInfo();
+  }
+
+}
