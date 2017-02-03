56661ec803ffe2e79c7e94318daed9e668379662
wfsovereign <wfsovereign@outlook.com>
2017-02-03 16:12:59 +0800

    [fyqj] replace use item list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ItemListComponent } from './list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
