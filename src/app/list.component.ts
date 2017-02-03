d644959e51f17f55cb759b6535f8a83cf02ae4ea
wfsovereign <wfsovereign@outlook.com>
2017-02-03 16:06:46 +0800

    [fyqj] extract list component

---
+++ src/app/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'item-list',
+  styleUrls: ['./list.component.scss'],
+  templateUrl: './list.component.html'
+})
+export class ItemListComponent {
+
+}
